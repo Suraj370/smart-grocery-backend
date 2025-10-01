4b7bcb68b203e9b1d65f393a7bb9d80009b4e94b
Suraj370 <surajpanda2077@gmail.com>
2025-10-01 12:07:06 +0530

    initial commit

---
+++ src/main/java/com/suraj/smartgroceryapp/SmartgroceryappApplication.java
@@ -0,0 +1,13 @@
+package com.suraj.smartgroceryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartgroceryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartgroceryappApplication.class, args);
+	}
+
+}
